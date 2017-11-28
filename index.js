e0c58921a0ad48bb07857c03b8c45e491c9b0428
Landon Kuhn <landon9720@gmail.com>
2017-11-28 11:35:08 -0800

    2

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const context = new AudioContext()
 const a = context.createScriptProcessor(0, 0, 1)
 
-const f = 440000
+const f = 440
 const omega = 2 * Math.PI * f / context.sampleRate
+let y0 = 0
+let y1 = Math.sin(omega)
+const c = 2 * Math.cos(omega)
+const k = .9999
 
 a.onaudioprocess = e => {
     const output = e.outputBuffer.getChannelData(0)
-    for (let i = 0; i < output.length; ++i) {
-        const n = context.sampleRate * e.playbackTime + i
-        output[i] = Math.sin(omega * n) * 2 - 1
+    for (let i = 0; i < output.length; i += 2) {
+        output[i + 0] = y0
+        output[i + 1] = y1
+        y0 = c * y1 - k * y0
+        y1 = c * y0 - k * y1
     }
 }
 
 a.connect(context.destination)
-setTimeout(() => context.close(), 1000)
+setTimeout(() => context.close(), 10000)
