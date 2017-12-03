d045653fc1672d988c639cb769ad044fad275163
Landon Kuhn <landon9720@gmail.com>
2017-12-02 17:03:04 -0800

    x

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import _ from 'lodash'
 import { context, F, play } from './context'
 import Monad from './Monad'
 import scriptNodeFactory from './scriptNodeFactory'
-import {sin, sum, seq, loop, gain, organ, shave, trim} from './monads'
+import { sin, sum, seq, loop, gain, organ, shave, trim } from './monads'
 
 const fs = _.range(80, 2000, 30)
 const tune = loop(seq(..._.map(fs, f => organ(f, 1))))
